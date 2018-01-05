2f68a396e9674a6f356c9d3b75f5418d22f16434
Marinés anton castello <mia_castello_k94@hotmail.com>
2018-01-05 18:34:44 +0100

    Cosas del Footer

--- YoureOnGen/YoureOnBootsTrap/Controllers/HomeController.cs
+++ YoureOnGen/YoureOnBootsTrap/Controllers/HomeController.cs
@@ -33,12 +33,5 @@ namespace YoureOnBootsTrap.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
