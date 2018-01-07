614eee57d9932d4e32b4da3d310afd21c2d2d7fb
Marinés anton castello <mia_castello_k94@hotmail.com>
2018-01-07 05:10:59 +0100

    Roles de usuarios

--- YoureOnGen/YoureOnBootsTrap/Startup.cs
+++ YoureOnGen/YoureOnBootsTrap/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using YoureOnBootsTrap.Models;
 
 [assembly: OwinStartupAttribute(typeof(YoureOnBootsTrap.Startup))]
 namespace YoureOnBootsTrap
@@ -9,6 +13,43 @@ namespace YoureOnBootsTrap
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CrearRoles();
+        }
+
+        private void CrearRoles()
+        {
+            string admin = "Administrador";
+            string moderador = "Moderador";
+
+            ApplicationDbContext db = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            // Comprobamos si no existe el rol previamente para crearlo
+            if (!roleManager.RoleExists(admin))
+            {
+                roleManager.Create(new IdentityRole(admin));
+
+                var user = new ApplicationUser();
+                user.UserName = "admin";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "Admin94*";
+                var chkUser = userManager.Create(user, userPWD);
+
+                // Le asignamos el rol de Administrador
+                if (chkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, admin);
+                }
+            }
+
+            // Creamos el rol de moderador
+            if (!roleManager.RoleExists(moderador))
+            {
+                roleManager.Create(new IdentityRole(moderador));
+            }
+                
         }
     }
 }
