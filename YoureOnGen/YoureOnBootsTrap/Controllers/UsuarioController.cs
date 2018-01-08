41f6b85021f15f8d59e9f65ebe203a013b8d0873
Débora Galdeano González <deb8192@gmail.com>
2018-01-08 12:59:24 +0100

    Resubido con las cosas que tenia eva y que habia subido

---
+++ YoureOnGen/YoureOnBootsTrap/Controllers/UsuarioController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using YoureOnGenNHibernate.EN.YoureOn;
+using YoureOnGenNHibernate.CEN.YoureOn;
+using YoureOnGenNHibernate.CAD.YoureOn;
+using WebApplication1.Models;
+using YoureOnBootsTrap.Models;
+
+
+namespace YoureOnBootsTrap.Controllers
+{
+    public class UsuarioController : BasicController
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            UsuarioCAD usu = new UsuarioCAD(session);
+            //var result = evCEN.get_IEventoCAD();
+            IEnumerable<UsuarioEN> list = usu.ReadAllDefault(0, int.MaxValue);
+            return View(list);
+        }
+
+        // GET: Usuario/Details/email
+        public ActionResult Details(String email)
+        {
+            SessionInitialize();
+            UsuarioEN usuarioen = new UsuarioCAD(session).ReadOIDDefault(email);
+            Usuario usu = new AssemblerUsuario().ConvertENToModelUI(usuarioen);
+            SessionClose();
+            return View(usu);
+        }
+
+
+
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            //redirigir a accountController
+            UsuarioEN en = new UsuarioEN();
+            return View(en);
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                UsuarioCEN nuevousuario = new UsuarioCEN();
+
+                //PD : VEIS , AQUI SE ASIGNA LA VARIABLE DE SESION, PORQUE CrearUser devuelve un id
+                System.Web.HttpContext.Current.Session["Identificador"] = nuevousuario.CrearUsuario(p_email: collection["Email"], p_nombre: collection["Nombre"], p_apellidos: collection["Apellidos"], p_fechaNac: new DateTime(2011, 6, 10), p_NIF: collection["NIF"],
+                    p_foto: "Foto", p_contrasenya: "1234Eva.", p_esVetado: false);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+
+
+
+
+        // GET: Usuario/Edit/email
+        public ActionResult Edit(String email)
+        {
+
+            SessionInitialize();
+            UsuarioEN conEN = new UsuarioCAD(session).ReadOIDDefault(email);
+            //com = new AssemblerUser().ConvertENToModelUI(conEN);
+            SessionClose();
+            return View(conEN);
+        }
+
+        // POST: Usuario/Edit/email
+        [HttpPost]
+        public ActionResult Edit(UsuarioEN u)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                UsuarioCEN cen = new UsuarioCEN();
+                cen.EditarPerfil(u.Email, u.Nombre, u.Apellidos, u.FechaNac, u.NIF, u.Foto, u.Contrasenya, u.EsVetado);
+                return RedirectToAction("Index", "Home");
+                //return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+
+                return View();
+            }
+        }
+
+
+
+
+        // GET: Usuario/Delete/email
+        public ActionResult Delete(string email)
+        {
+
+            try
+            {
+                // TODO: Add delete logic here
+                //int idCategoria = -1;
+                SessionInitialize();
+                UsuarioCAD artCAD = new UsuarioCAD(session);
+                UsuarioCEN cen = new UsuarioCEN(artCAD);
+                UsuarioEN artEN = cen.CargarPerfil(email);
+                Usuario art = new AssemblerUsuario().ConvertENToModelUI(artEN);
+                //idCategoria = art.IdCategoria;
+                SessionClose();
+
+                new UsuarioCEN().Destroy(email);
+
+                //Request.Abort();
+                //Session.Clear();
+                //Session.Abandon();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: Usuario/Delete/email
+        [HttpPost]
+        public ActionResult Delete(string email, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+    }
+}
---
+++ YoureOnGen/YoureOnBootsTrap/Models/AssemblerUsuario.cs
@@ -0,0 +1,42 @@
+﻿using YoureOnBootsTrap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YoureOnGenNHibernate.EN.YoureOn;
+using YoureOnGenNHibernate.Enumerated.YoureOn;
+using System.Web.Mvc;
+//using YoureOnGenNHibernate.EN.YoureOn.UsuarioEN;
+//using WebApplication1.Models;
+
+
+namespace WebApplication1.Models
+{
+    public class AssemblerUsuario : Controller
+    {
+        public Usuario ConvertENToModelUI(UsuarioEN en)
+        {
+            Usuario usi = new Usuario();
+            usi.Email = en.Email;
+            usi.Nombre = en.Nombre;
+            usi.Apellidos = en.Apellidos;
+            usi.FechaNac = en.FechaNac;
+            usi.NIF = en.NIF;
+            usi.Foto = en.Foto;
+            usi.Contrasenya = en.Contrasenya;
+
+            return usi;
+
+
+        }
+        public IList<Usuario> ConvertListENToModel(IList<UsuarioEN> ens)
+        {
+            IList<Usuario> usus = new List<Usuario>();
+            foreach (UsuarioEN en in ens)
+            {
+                usus.Add(ConvertENToModelUI(en));
+            }
+            return usus;
+        }
+    }
+}
